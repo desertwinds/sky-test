f5ddffef4263c0ced8ecf5341d67bfc28a9c6dc8
Javier <javiantonini@gmail.com>
2016-10-05 10:17:33 +0100

    Getting the bill info into the controller.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,20 @@
 class BillsController < ApplicationController
+  
   def index
-    
+    @json_response = get_bill
   end
+
+  private 
+
+    def get_bill
+      api_response = ApiCaller.new.bill
+      json_response = {}
+      if (api_response.code == 200 && api_response.headers["content-type"] && api_response.headers["content-type"] == "application/json")
+        json_response = JSON.parse(api_response.body)
+      else
+        json_response["errors"] = "Error on the API endpoint."
+      end
+
+      json_response
+    end
 end
