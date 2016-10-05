cb72b922014428aa1d33b242bcfedb3af2356378
Javier <javiantonini@gmail.com>
2016-10-05 18:39:19 +0100

    Changed the lib position to app path, since it is causing troubles in deployment.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module SkyTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
-    #Add our libs to be loaded
-    config.autoload_paths << "#{Rails.root}/lib"
+
   end
 end
