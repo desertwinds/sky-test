0389b859966ddc329b7f2aacb134cbf128e9fd93
Javier <javiantonini@gmail.com>
2016-10-05 17:51:56 +0100

    Added bootstrap fix to application.js. Added fonts to assets pipeline.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module SkyTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #Add our libs to be loaded
     config.autoload_paths += %W(#{config.root}/lib)
+
+    #Add our fonts to the assets pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
